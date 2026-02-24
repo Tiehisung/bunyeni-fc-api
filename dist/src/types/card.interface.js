0ac085767939db8956cb9c9ba3459941c8f5844b
Alhassan Ibrahim <tiehisungai@gmail.com>
2026-02-24 00:52:19 +0000

    rename from manager to staff

--- dist/src/types/card.interface.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ECardType = void 0;
-var ECardType;
-(function (ECardType) {
-    ECardType["YELLOW"] = "yellow";
-    ECardType["RED"] = "red";
-})(ECardType || (exports.ECardType = ECardType = {}));
--- dist/src/types/log.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ELogSeverity = void 0;
-var ELogSeverity;
-(function (ELogSeverity) {
-    ELogSeverity["INFO"] = "info";
-    ELogSeverity["WARNING"] = "warning";
-    ELogSeverity["CRITICAL"] = "critical";
-})(ELogSeverity || (exports.ELogSeverity = ELogSeverity = {}));
